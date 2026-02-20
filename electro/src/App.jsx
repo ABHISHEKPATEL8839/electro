52010fc62fa241c991a0f9ee2327413540854018
ABHISHEKPATEL8839 <abhishekpatel71773@gmail.com>
2026-02-20 12:47:19 +0530

    hello

---
+++ electro/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AllRoute from './assets/components/AllRoute'
+
+const App = () => {
+  return (
+    <>
+    
+    <AllRoute/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ electro/src/assets/components/AllRoute.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from '../pages/Home'
+const AllRoute = () => {
+  return (
+   <>
+   
+   <Home/>
+   
+   
+   
+   
+   </>
+  )
+}
+
+export default AllRoute
